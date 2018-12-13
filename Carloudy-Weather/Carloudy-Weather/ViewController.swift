87706401f67d26b8e95a1e8cefb573547b97cb6a
Zijia Zhai <zijiazhai@gmail.com>
2018-12-13 13:18:37 -0600

    First push

---
+++ Carloudy-Weather/Carloudy-Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carloudy-Weather
+//
+//  Created by Zijia Zhai on 12/13/18.
+//  Copyright © 2018 cognitiveAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
